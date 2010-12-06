121d647887f510ac3a991d453fc1f466fb5fa091
Grzegorz <czacha.osw@gmail.com>
2010-12-06 22:04:14 +0100

    fixed problems

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,19 +1,12 @@
 class Bet < ActiveRecord::Base
 belongs_to :duel
 belongs_to :user
-<<<<<<< HEAD
 before_destroy :refund_gold
 validate :duel_start_date_cannot_be_in_the_past
 
-protected
- def refund_gold
-=======
-before_destroy :back_gold
-validate :duel_start_date_cannot_be_in_the_past
 
 protected
- def back_gold
->>>>>>> f1ed896c65919abd0878f6063e33466881653861
+ def refund_gold
    @duel = Duel.find_by_id(self.duel_id)
    if @duel.finished == false then
    @user = User.find_by_id(self.user_id)
